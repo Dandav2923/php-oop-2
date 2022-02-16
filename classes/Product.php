49af491437ed307da29e31c55b917ebe9acab685
Dandav2923 <daniele.emili23@gmail.com>
2022-02-16 15:36:41 +0100

    struttura dati getter e setter

--- classes/Product.php
+++ classes/Product.php
@@ -1,6 +1,121 @@
 <?php 
+class Product 
+{
+    protected $id;
+    protected $productName;
+    protected $productPrice;
+    protected $productType;
+    protected $productBrand;
+    protected $productDescription;
 
 
+    public function __construct($productName, $productType, $productPrice)
+    {
+        $this->productName = $productName;
+        $this->productType = $productType;
+        $this->productPrice = $productPrice;
+    }
+    
+
+    /**
+     * Get the value of productName
+     */ 
+    public function getProductName()
+    {
+        return $this->productName;
+    }
+
+    /**
+     * Set the value of productName
+     *
+     * 
+     */ 
+    public function setProductName($productName)
+    {
+        $this->productName = $productName;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of productPrice
+     */ 
+    public function getProductPrice()
+    {
+        return $this->productPrice;
+    }
+
+    /**
+     * Set the value of productPrice
+     *
+     * 
+     */ 
+    public function setProductPrice($productPrice)
+    {
+        $this->productPrice = $productPrice;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of productType
+     */ 
+    public function getProductType()
+    {
+        return $this->productType;
+    }
+
+    /**
+     * Set the value of productType
+     *
+     * 
+     */ 
+    public function setProductType($productType)
+    {
+        $this->productType = $productType;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of productBrand
+     */ 
+    public function getProductBrand()
+    {
+        return $this->productBrand;
+    }
+
+    /**
+     * Set the value of productBrand
+     *
+     * 
+     */ 
+    public function setProductBrand($productBrand)
+    {
+        $this->productBrand = $productBrand;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of productDescription
+     */ 
+    public function getProductDescription()
+    {
+        return $this->productDescription;
+    }
+
+    /**
+     * Set the value of productDescription
+     *
+     */ 
+    public function setProductDescription($productDescription)
+    {
+        $this->productDescription = $productDescription;
+
+        return $this;
+    }
+}
 
 
 
--- classes/User.php
+++ classes/User.php
@@ -1,6 +1,98 @@
 <?php 
 
+class User 
+{
+    protected $userName;
+    protected $userLastName;
+    protected $userEmail;
+    protected $userPaymentMethod;
+    public function __construct($userName, $userLastName) {
+        $this->userName = $userName;
+        $this->userLastName = $userLastName;
+    }
 
+    
+
+    /**
+     * Get the value of userName
+     */ 
+    public function getUserName()
+    {
+        return $this->userName;
+    }
+
+    /**
+     * Set the value of userName
+     *
+     * 
+     */ 
+    public function setUserName($userName)
+    {
+        $this->userName = $userName;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of userLastName
+     */ 
+    public function getUserLastName()
+    {
+        return $this->userLastName;
+    }
+
+    /**
+     * Set the value of userLastName
+     *
+     * 
+     */ 
+    public function setUserLastName($userLastName)
+    {
+        $this->userLastName = $userLastName;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of userEmail
+     */ 
+    public function getUserEmail()
+    {
+        return $this->userEmail;
+    }
+
+    /**
+     * Set the value of userEmail
+     *
+     * 
+     */ 
+    public function setUserEmail($userEmail)
+    {
+        $this->userEmail = $userEmail;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of userPaymentMethod
+     */ 
+    public function getUserPaymentMethod()
+    {
+        return $this->userPaymentMethod;
+    }
+
+    /**
+     * Set the value of userPaymentMethod
+     *
+     * 
+     */ 
+    public function setUserPaymentMethod($userPaymentMethod)
+    {
+        $this->userPaymentMethod = $userPaymentMethod;
+
+        return $this;
+    }
+}
 
 
 
