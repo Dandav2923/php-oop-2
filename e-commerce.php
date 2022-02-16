a3a948ee091b810b037d162d39e98946ba13dd13
Dandav2923 <daniele.emili23@gmail.com>
2022-02-16 15:58:03 +0100

    pre hotwell

--- e-commerce.php
+++ e-commerce.php
@@ -2,8 +2,12 @@
 require_once __DIR__ . '/classes/Product.php';
 require_once __DIR__ . '/classes/User.php';
 
-
-
-
+try {
+    $product = new Product('Magic Keyboard', 99, null, 'Apple', null);
+    $product->setProductType('Hardware');
+    var_dump($product);
+} catch (\Throwable $th) {
+    //throw $th;
+}
 
 ?>
